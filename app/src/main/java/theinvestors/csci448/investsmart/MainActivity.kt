39a41ad574e880d5f70399d1238d2088505305fe
alperenugus <alperenugus@gmail.com>
2020-03-07 19:18:08 -0700

    Added Fragment classes for each fragment, added their lifecycle methods

--- app/src/main/java/theinvestors/csci448/investsmart/MainActivity.kt
+++ app/src/main/java/theinvestors/csci448/investsmart/MainActivity.kt
@@ -2,11 +2,20 @@ package theinvestors.csci448.investsmart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import theinvestors.csci448.investsmart.ui.HomeScreenFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = HomeScreenFragment()
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+
     }
 }
---
+++ app/src/main/java/theinvestors/csci448/investsmart/ui/SettingsFragment.kt
@@ -0,0 +1,76 @@
+package theinvestors.csci448.investsmart.ui
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import theinvestors.csci448.investsmart.R
+
+private const val logTag: String = "SettingsFragment"
+
+class SettingsFragment: Fragment() {
+
+    override fun onAttach(context: Context?) {
+        Log.d(logTag, "onAttach() called")
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(logTag, "onCreate() called")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(logTag, "onCreateView() called")
+
+        val view: View = inflater.inflate(R.layout.settings, container, false)
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        Log.d(logTag, "onActivityCreated() called")
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(logTag, "onStart() called")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(logTag, "onResume() called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(logTag, "onPause() called")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(logTag, "onStop() called")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.d(logTag, "onDestroyView() called")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.d(logTag, "onDestroy() called")
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        Log.d(logTag, "onDetach() called")
+        super.onDetach()
+    }
+}
